c1be186c23262a3ecba08226b43d3e3ee0727f88
gabsoueu <101360255+gabsoueu@users.noreply.github.com>
2022-08-04 16:15:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,13 +56,13 @@ function draw() {
   //   }
   // }
 
-   if (frameCount % 80 == 0) {
-     if (select_sprites == 1) {
-      createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-     }
-   }
+  // if (frameCount % 80 == 0) {
+  //   if (select_sprites == 1) {
+  //    createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }
+  // }
 
   // if (frameCount / 80 == 0) {
   //   if (select_sprites == 1) {
@@ -74,15 +74,15 @@ function draw() {
   //   }
   // }
 
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+  }
 
 
 
